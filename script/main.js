2efe36e246c99436de4f591d74266d88283483a9
O7Mejri <oussama.mejri@enis.tn>
2021-04-16 17:57:38 +0100

    .

--- script/main.js
+++ script/main.js
@@ -17,3 +17,53 @@ exit.addEventListener('click', function (e) {
     menu.classList.toggle('mobile-menu-show')
     e.preventDefault();
 });
+
+//      **********          Podcast Player            *********
+var playBtn = document.getElementById("play-btn");
+var pauseBtn = document.getElementById("pause-btn");
+var podProgress = document.getElementById("pod-progress");
+var podTime = document.getElementById("pod-time")
+var podcast = document.getElementById("pod-audio");
+var intervalId;
+
+
+playBtn.addEventListener('click', function(e) {
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+    podcast.play();
+
+    intervalId = window.setInterval(function(){
+        progressBar();
+      }, 1000);
+
+    e.preventDefault();
+});
+pauseBtn.addEventListener('click', function(e) {
+    pauseBtn.style.display = "none";
+    playBtn.style.display = "block";
+    podcast.pause();
+    clearInterval(intervalId);
+    e.preventDefault();
+});
+
+function progressBar() {
+    var percent = (podcast.currentTime/podcast.duration) * 100;
+    podProgress.style.width = percent + "%";
+
+    var time = Math.floor(podcast.currentTime);
+    var sec = time % 60;
+    var min = Math.floor(time / 60);    
+    if(sec<10){
+        podTime.innerHTML = "0" + min + ":0" + sec;
+        
+    }else{
+        podTime.innerHTML = "0" + min + ":" + sec;}
+}
+
+
+
+
+
+
+
+
